ba41b1cc9e7bf8f6980dc43620c6056e6445f266
dots111 <alter001@163.com>
2019-12-01 01:50:28 +0800

    init

---
+++ main.c
@@ -0,0 +1,27 @@
+#include "stm8s.h"
+
+int main(void)
+{
+    return 0;
+}
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param file: pointer to the source file name
+  * @param line: assert_param error line source number
+  * @retval : None
+  */
+void assert_failed(u8* file, u32 line)
+{
+  
+  UNUSED(file);
+  UNUSED(line);
+
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
