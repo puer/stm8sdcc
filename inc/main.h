bae5f3ada85e8441908d12614c2a956306f61028
dots111 <alter001@163.com>
2019-12-06 15:29:28 +0800

    put interrupt service routine to main.c as sdcc required

--- inc/main.h
+++ inc/main.h
@@ -1,15 +1,9 @@
 #include "stm8s.h"
 
-#define LED_PIN GPIO_PIN_4
-#define LED_PORT GPIOA
-
+#define LED_PIN GPIO_PIN_5
+#define LED_PORT GPIOB
 
 #define RE_PORT GPIOC
 #define RE_PSBTN_PIN GPIO_PIN_3
 #define RE_A_PIN GPIO_PIN_4
 #define RE_B_PIN GPIO_PIN_5
-
-inline void LEDOn(void);
-inline void LEDOff(void);
-uint32_t LSIMeasurment(void);
-void Delay(uint16_t nCount);
--- src/main.c
+++ src/main.c
@@ -1,32 +1,51 @@
 #include "stm8s.h"
 #include "main.h"
+#include "stdio.h"
 
-#define TIM4_PERIOD       124
+#define TIM4_PERIOD 124
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 __IO uint32_t TimingDelay = 0;
 /* Private function prototypes -----------------------------------------------*/
 void Delay(__IO uint32_t nTime);
-void TimingDelay_Decrement(void);
 static void TIM4_Config(void);
+static void UART1_Config(void);
+
+INTERRUPT_HANDLER(TIM4_UPD_OVF_IRQHandler, 23)
+{
+  /* In order to detect unexpected events during development,
+     it is recommended to set a breakpoint on the following instruction.
+  */
+  if (TimingDelay != 0x00)
+  {
+    TimingDelay--;
+  }
+  TIM4->SR1 = (uint8_t)(~TIM4_SR1_UIF);
+}
 
 int main(void)
 {
-  CLK->CKDIVR = ((uint8_t) 0x0);
+  CLK->CKDIVR = ((uint8_t)0x0);
   TIM4_Config();
+  UART1_Config();
 
   enableInterrupts();
 
+  LED_PORT->DDR |= (uint8_t)LED_PIN;
+  LED_PORT->CR1 |= (uint8_t)LED_PIN;
+  LED_PORT->CR2 &= (uint8_t)(~(LED_PIN));
 
   while (1)
   {
-
+    printf("Hello\n");
+    LED_PORT->ODR &= (uint8_t)(~LED_PIN);
     Delay(100);
-
+    LED_PORT->ODR |= (uint8_t)LED_PIN;
+    Delay(1000);
   }
 }
 
-static void TIM4_Config(void) 
+static void TIM4_Config(void)
 {
   /* div by 128 count = 125 => 1ms UEV */
   TIM4->PSCR = (uint8_t)(0x07);
@@ -39,10 +58,25 @@ static void TIM4_Config(void)
   TIM4->CR1 |= TIM4_CR1_ARPE;
   /* enable tim4 */
   TIM4->CR1 |= TIM4_CR1_CEN;
-
-    
 }
 
+static void UART1_Config(void)
+{
+  UART1->CR2 = UART1_CR2_TEN | UART1_CR2_REN;
+  // 1 Stop Bit
+  // UART1->CR3;
+  // 9600 baud
+  UART1->BRR2 = 0x03;
+  UART1->BRR1 = 0x68;
+}
+
+int putchar(int c)
+{
+  while (!(UART1->SR & UART1_SR_TXE))
+    ;
+  UART1->DR = (uint8_t)c;
+  return c;
+}
 /**
   * @brief  Inserts a delay time.
   * @param  nTime: specifies the delay time length, in milliseconds.
@@ -52,20 +86,8 @@ void Delay(__IO uint32_t nTime)
 {
   TimingDelay = nTime;
 
-  while (TimingDelay != 0);
-}
-
-/**
-  * @brief  Decrements the TimingDelay variable.
-  * @param  None
-  * @retval None
-  */
-void TimingDelay_Decrement(void)
-{
-  if (TimingDelay != 0x00)
-  {
-    TimingDelay--;
-  }
+  while (TimingDelay != 0)
+    ;
 }
 
 #ifdef USE_FULL_ASSERT
@@ -76,9 +98,9 @@ void TimingDelay_Decrement(void)
   * @param line: assert_param error line source number
   * @retval : None
   */
-void assert_failed(u8* file, u32 line)
+void assert_failed(u8 *file, u32 line)
 {
-  
+
   UNUSED(file);
   UNUSED(line);
 
